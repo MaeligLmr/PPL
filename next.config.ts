7beae9a14655eeb3033b5ed26b56bd36fabbbb43
Lamarre <maelig.lamarre49@gmail.com>
2026-02-07 11:19:17 +0100

    (chore) again a build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
   distDir: "docs",
-  basePath: isProd ? "/PPL" : "",
-  assetPrefix: isProd ? "/PPL/" : "",
+  basePath: "/PPL",
+  assetPrefix: "/PPL/",
 };
 
 export default nextConfig;
